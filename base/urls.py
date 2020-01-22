bf80780901616f5223bb9cb11d1e5053738ad5e5
Pruthvi_Patel <pruthvipatel2807@gmail.com>
2020-01-22 17:00:23 +0530

    MyWebsite update 0.1.2

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('message', views.message, name='message')
 ]
--- base/views.py
+++ base/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def message(request):
+    name = request.POST["name"]
+    subject = request.POST["subject"]
+    emailid = request.POST["emailid"]
+    usrmessage = request.POST["usrmessage"]
+    return render(request, 'UsrMessage.html', {'name': name, 'subject': subject, 'emailid': emailid, 'usrmessage': usrmessage})
