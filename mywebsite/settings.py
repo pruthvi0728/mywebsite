9624c251383dbdf66aeda37cbf216c9cf8c3881a
Pruthvi_Patel <pruthvipatel2807@gmail.com>
2020-01-23 14:24:53 +0530

    MyWebsite update 0.2.1

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
