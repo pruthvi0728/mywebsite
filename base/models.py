ba13a88a5eca09b0aa99dae5ab2f18db7ffec4af
Pruthvi_Patel <pruthvipatel2807@gmail.com>
2020-01-23 12:36:57 +0530

    MyWebsite update 0.2.0

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=200)
+    emailid = models.CharField(max_length=100)
+    usrmessage = models.TextField()
--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Message
 # Create your views here.
 
 
