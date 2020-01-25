bfe171979148e7a91137b9d363db207fe49f26be
Pruthvi_Patel <pruthvipatel2807@gmail.com>
2020-01-25 10:15:20 +0530

    MyWebsite update 0.4.0

---
+++ chatapp/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+# Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatmain')
+        else:
+            messages.info(request, "Invalid Credentials")
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        emailid = request.POST['emailid']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.info(request, "Password mismatch")
+            return redirect('register')
+        elif User.objects.filter(username=username).exists():
+            messages.info(request, "Username Taken")
+            return redirect('register')
+        elif User.objects.filter(email=emailid).exists():
+            messages.info(request, "Already Have Account try login")
+            return redirect('login')
+        else:
+            user = User.objects.create_user(username=username, email=emailid, password=password1, first_name=firstname,
+                                            last_name=lastname)
+            user.save()
+            messages.info(request, "Account Created Successfully")
+            return redirect('login')
+    else:
+        return render(request, 'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
+
+def chatmain(request):
+    return render(request, 'chatmain.html')
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'chatapp',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'base/templates/base')],
+        'DIRS': [os.path.join(BASE_DIR, 'base/templates/base'), os.path.join(BASE_DIR, 'chatapp/templates/chatapp')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
