9f116f9764fa774c2b14e4b537819e308cc2b850
Pruthvi_Patel <pruthvipatel2807@gmail.com>
2020-02-02 22:44:50 +0530

    MyWebsite update 0.9.0 deploy 0.0.6

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'x)_d)c3mtmctwuuoq0-87_8kh$+n)e
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['*']
 
 
